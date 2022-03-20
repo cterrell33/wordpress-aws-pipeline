136af7092a4b22100f27339c6ef1d12bea3b1432
Christopher Terrell <cterrell@kingdomtech.net>
2022-03-20 09:32:58 -0400

    added aws cli bash script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "**THIS SCRIPT VALIDATES AND CREATES/UPDATES THE CF STACK**"
+
+aws cloudformation deploy --stack-name $1 --template-file template.yml --parameter-overrides file://parameters.json --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --no-execute-changeset
