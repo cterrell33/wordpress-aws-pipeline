44e37e01d7e3933d678ad8d1f48617c68ae07021
Christopher Terrell <cterrell@kingdomtech.net>
2022-03-20 09:45:23 -0400

    added cloudformation template

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 echo "**THIS SCRIPT VALIDATES AND CREATES/UPDATES THE CF STACK**"
 
-aws cloudformation deploy --stack-name $1 --template-file template.yml --parameter-overrides file://parameters.json --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --no-execute-changeset
+aws cloudformation deploy --stack-name $1 --template-file template.yaml --parameter-overrides file://parameters.json --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM --no-execute-changeset
